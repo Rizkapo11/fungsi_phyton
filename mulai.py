5e8cec80dd7ef583c890ad9a7e53b36426cc6172
rizka <rizkaputriocta11@gmail.com>
2020-09-29 09:38:11 +0700

    review fungsi_phyton

---
+++ mulai.py
@@ -0,0 +1,16 @@
+#SEKUENSUAL = EKSEKUSI BERURUTAN
+print ('Rizka putri octarini')
+print ('11 oktober 2001')
+print ('Bekasi')
+
+#PERCABANGAN = EKSEKUSI TERPILIH
+ingin_bermain = True
+if ingin_bermain :
+    print ('bola basket')
+else :
+    print ('bola voli')
+
+#PENGULANGAN
+jumlah_polisi = (5)
+for index_polisi  in range (1, 6): #jumlah perulangan 6 - 1 = 4
+    print (f'siap gerak #{index_polisi}')
