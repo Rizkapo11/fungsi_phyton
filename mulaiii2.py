cae37843fa6dda71d0283a964894ca1da9e6f01e
rizka <rizkaputriocta11@gmail.com>
2020-09-29 16:22:13 +0700

    review fungsi_phyton mulaiiii2

---
+++ mulaiii2.py
@@ -0,0 +1,35 @@
+print ('/n data tipe skalar')
+
+anak1 = 'dena'
+anak2 = 'tami'
+anak3 = 'rafli'
+anak4 = 'rizka'
+anak5 = 'nazriel'
+
+print (anak1)
+print (anak2)
+print (anak3)
+print (anak4)
+print (anak5)
+
+print ('\ntipe data list/array')
+anak = []
+anak.append ('dena')
+anak.append ('tami')
+anak.append ('rafli')
+anak.append ('rizka')
+anak.append ('nazriel')
+print (anak)
+anak = ['dena', 'tami', 'rafli', 'rizka', 'nazriel']
+print (anak)
+anak.append ('gausahada')
+print (anak)
+print ('\n sapa anak')
+print (f'hai {anak[3]} !')
+print ('\nsapa semua anak')
+for a in anak :
+    print (f'hai {a} !')
+print ('\n sapa semua anak cara ribet')
+for a in range (0, len (anak)) :
+    print (f'{a+1}.hai {anak [a]}')
+
